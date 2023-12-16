4072ce000377e9b4cdd54abe8d55886854716ec7
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-12-16 15:35:29 +0500

    Часть 1

--- tests/test_demoqa_practice_form.py
+++ tests/test_demoqa_practice_form.py
@@ -1,30 +1,41 @@
 import os
 from selene import browser, have, be
+
+from Python_hw_10.pages.registration_page import RegistrationPage
+from Python_hw_10.resources.data_user import DataUser
 from Python_hw_10.resources.path import CURRENT_DIR
 
 
 def test_fill_form(browser_config):
-    browser.open('/automation-practice-form')
-    browser.element('#firstName').click().should(be.blank).type('Ivan')
-    browser.element('#lastName').click().should(be.blank).type('Ivanov')
-    browser.element('#userEmail').click().should(be.blank).type('name@example.com')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').click().should(be.blank).type('8999999999')
-    browser.element('[id="dateOfBirthInput"]').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('[value="1996"]').click()
-    browser.element('[class*=month-select]').click()
-    browser.element('[class*=month-select] [value="1"]').click()
-    browser.element('[class*=day--022]').click()
-    browser.element('#subjectsInput').click().type('Eng').press_tab()
-    browser.element('[for=hobbies-checkbox-1]').click()
-    browser.element('[for=hobbies-checkbox-2]').click()
-    browser.element('[for=hobbies-checkbox-3]').click()
-    browser.element('[type=file]').send_keys(os.path.join(CURRENT_DIR, 'img.png'))
-    browser.element('#currentAddress').click().should(be.blank).type('Baker Street')
-    browser.element('#react-select-3-input').type('Haryana').press_enter()
-    browser.element('#react-select-4-input').type('Panipat').press_enter()
-    browser.element('#submit').press_enter()
-    browser.all('tbody tr td')[1::2].should(
-        have.texts('Ivan Ivanov', 'name@example.com', 'Male', '8999999999', '22 February,1996', 'English',
-                   'Sports, Reading, Music', 'img.png', 'Baker Street', 'Haryana Panipat'))
+    registration_page = RegistrationPage()
+    dataUser = DataUser()
+
+    registration_page.open_url('/automation-practice-form')
+
+    registration_page.fill_first_name(dataUser.firstName)
+
+    registration_page.fill_last_name(dataUser.lastName)
+
+    registration_page.fill_email(dataUser.email)
+
+    registration_page.choice_gender()
+
+    registration_page.fill_number_phone(dataUser.numberPhone)
+
+    registration_page.choice_birthday(dataUser.birthday)
+
+    registration_page.fill_subject(dataUser.subjects)
+
+    registration_page.choice_hobbies()
+
+    registration_page.upload_img(CURRENT_DIR, 'img.png')
+
+    registration_page.fill_address(dataUser.address)
+
+    registration_page.state_city(dataUser.stateCity[0], dataUser.stateCity[1])
+
+    registration_page.click_submit()
+
+    registration_page.should_have_registered('Ivan', 'Ivanov', 'name@example.com', 'Male',  '8999999999',
+                                             '22 February,1996', 'English', 'Sports, Reading, Music', 'img.png',
+                                             'Baker Street', 'Haryana Panipat')
