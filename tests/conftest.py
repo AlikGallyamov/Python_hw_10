872dbcd2bc60a79588dfea8c2c91d0f25e4be0e0
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-12-21 14:25:24 +0500

    Поправил импорты

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,29 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene import browser
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_config():
-
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": True
+            "enableVideo": False
         }
     }
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-    browser = Browser(Config(driver))
+        command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
+        options=options)
 
-
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
-    # driver_options = webdriver.ChromeOptions()
-    # driver_options.add_argument('--headless')
-    # browser.config.driver_options = driver_options
-    # browser.config.window_width = 1900
-    # browser.config.window_height = 950
 
     yield
 
