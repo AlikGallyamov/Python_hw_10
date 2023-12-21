e8c0320066376502412dfc7582f2716ea444ae2b
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-12-21 17:36:20 +0500

    Подключил allure отчеты

--- Python_hw_10/pages/registration_page.py
+++ Python_hw_10/pages/registration_page.py
@@ -1,20 +1,28 @@
 from selene import browser, be, have
-
+import allure
 
 from Python_hw_10.controls import resource
 from datetime import datetime
 
+from Python_hw_10.data.user import DataUser
+from Python_hw_10.data.user import dataUser
+
 
 class RegistrationPage:
+
+    @allure.step(f"Заполняем имя {dataUser.firstName}")
     def fill_first_name(self, dataUser):
         browser.element('#firstName').click().should(be.blank).type(dataUser.firstName)
 
+    @allure.step(f"Заполняем фамилию {dataUser.lastName}")
     def fill_last_name(self, dataUser):
         browser.element('#lastName').click().should(be.blank).type(dataUser.lastName)
 
+    @allure.step(f"Заполняем email {dataUser.email}")
     def fill_email(self, dataUser):
         browser.element('#userEmail').click().should(be.blank).type(dataUser.email)
 
+    @allure.step(f"Выбираем пол {dataUser.gender}")
     def choice_gender(self, dataUser):
         gender_dict = {
             'Male': 1,
@@ -23,9 +31,11 @@ class RegistrationPage:
         }
         browser.element(f'[for="gender-radio-{gender_dict[dataUser.gender]}"]').click()
 
+    @allure.step(f"Заполняем номер телефона {dataUser.phoneNumber}")
     def fill_number_phone(self, dataUser):
         browser.element('#userNumber').click().should(be.blank).type(dataUser.phoneNumber)
 
+    @allure.step(f"Заполняем дату рождения {dataUser.dateBirth}")
     def choice_birthday(self, dataUser):
         date_text = dataUser.dateBirth
         date_format = '%d %B,%Y'
@@ -40,27 +50,34 @@ class RegistrationPage:
         browser.element(f'[class*=month-select] [value="{int(date_formated[1]) - 1}"]').click()
         browser.element(f'[class*=day--{date_formated[2]}]').click()
 
+    @allure.step(f"Заполняем subject {dataUser.subject}")
     def fill_subject(self, dataUser):
         browser.element('#subjectsInput').click().type(dataUser.subject).press_tab()
 
-    def choice_hobbies(self, dataUser):
+    @allure.step("Выбираем все хобби")
+    def choice_hobbies(self):
         browser.element('[for=hobbies-checkbox-1]').click()
         browser.element('[for=hobbies-checkbox-2]').click()
         browser.element('[for=hobbies-checkbox-3]').click()
 
+    @allure.step("Загружаем фото")
     def upload_img(self, dataUser):
         browser.element('[type=file]').send_keys(resource.path(dataUser.fileName))
 
+    @allure.step(f"Заполняем адрес {dataUser.address}")
     def fill_address(self, dataUser):
         browser.element('#currentAddress').should(be.blank).type(dataUser.address)
 
+    @allure.step(f"Заполняем штат: {dataUser.state} и город {dataUser.city}")
     def state_city(self, dataUser):
         browser.element('#react-select-3-input').type(dataUser.state).press_enter()
         browser.element('#react-select-4-input').type(dataUser.city).press_enter()
 
+    @allure.step("Открываем страницу")
     def open_url(self):
         browser.open('/automation-practice-form')
 
+    @allure.step("Нажимаем submit")
     def click_submit(self):
         browser.element('#submit').press_enter()
 
@@ -72,12 +89,14 @@ class RegistrationPage:
         self.fill_number_phone(dataUser)
         self.choice_birthday(dataUser)
         self.fill_subject(dataUser)
-        self.choice_hobbies(dataUser)
+        self.choice_hobbies()
         self.upload_img(dataUser)
         self.fill_address(dataUser)
         self.state_city(dataUser)
         self.click_submit()
 
+    dataUser = DataUser
+    @allure.step("Сравниваем результат")
     def should_have_registered(self, dataUser):
         browser.all('tbody tr td').should(
             have.texts('Student Name', f'{dataUser.firstName} {dataUser.lastName}', 'Student Email', dataUser.email,
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,22 +7,22 @@ from selene import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_config():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
-
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-
-    browser.config.driver = driver
+    # options = Options()
+    # selenoid_capabilities = {
+    #     "browserName": "chrome",
+    #     "browserVersion": "100.0",
+    #     "selenoid:options": {
+    #         "enableVNC": True,
+    #         "enableVideo": False
+    #     }
+    # }
+    #
+    # options.capabilities.update(selenoid_capabilities)
+    # driver = webdriver.Remote(
+    #     command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+    #     options=options)
+    #
+    # browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
 
     yield
