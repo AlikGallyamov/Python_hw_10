94c1e289540ae18c81ce324f06f7afa2f2f7ec59
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2023-12-21 19:51:03 +0500

    добавил в гитигнор папки с аллюром, resources вынес в корень проекта

--- Python_hw_10/controls/resource.py
+++ Python_hw_10/controls/resource.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 
 def path(picture_name):
-    return str(Path(__file__).parent.parent.joinpath(f'resources/{picture_name}'))
+    return str(Path(__file__).parent.parent.parent.joinpath(f'resources/{picture_name}'))
 
 
